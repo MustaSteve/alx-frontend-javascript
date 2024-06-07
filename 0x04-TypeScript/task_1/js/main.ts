2399d5c51a09de08d25297aafbd943382936b4ef
MustaSteve <avineusq@gmail.com>
2024-06-07 19:23:45 +0200

    Tasks all

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,60 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    location: string;
+    yearsOfExperience?: number;
+    [key: string]: any
+}
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const teacher1: Teacher = {
+    firstName: 'Frank',
+    lastName: 'Donald',
+    fullTimeEmployee: true,
+    location: 'New Jersey',
+    contract: false,
+}
+
+console.log(teacher1);
+
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Smith',
+    location: 'America',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+
+console.log(director1);
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => `${firstName[0]}. ${lastName}`;
+
+console.log(printTeacher('john', 'doe'));
+
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+}
+
+interface StudentClassConstructorInterface {
+    new(firstName: string, lastName: string): StudentClassInterface; 
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
